e69a690fdaed6e73d70c21c5423a154c4281dac1
root <root@ip-172-31-3-85.ap-south-1.compute.internal>
2019-12-10 11:25:27 +0000

    server files update

---
+++ myproject.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+from one import test
+
+app = Flask(__name__)
+api = Api(app)
+
+api.add_resource(test.HelloWorld,'/')
+
+if __name__ == "__main__":
+	app.debug = True
+	app.run(host='0.0.0.0')
+
